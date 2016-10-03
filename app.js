ed6d7c02e0fe5698aea8647ccf8cbbbde1011458
kcirekcom <erick.f.mock@gmail.com>
2016-10-03 14:06:35 -0700

    started js file

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var productImages = [];
+
+function Image() {
+  this.imageName = imageName;
+  this.filePath = filePath;
+  this.numberTimesDisplayed = 0;
+  this.numberTimesClicked = 0;
+  this.displayRandImages = function(){
+    for (var i = 0; i < productImages.length; i++) {
+      this.randCustPe.push(Math.random(productImages[i])
+    }
+  };
+}
